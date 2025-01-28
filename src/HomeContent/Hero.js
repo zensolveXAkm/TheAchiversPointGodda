f5eb1a67bae2bb3b1ee80d9eaf72dd0e1131b2ce
zensolveXAkm <carezensolve@gmail.com>
2025-01-28 14:38:34 +0530

    Update Hero.js

--- src/HomeContent/Hero.js
+++ src/HomeContent/Hero.js
@@ -11,7 +11,7 @@ const HeroSection = () => {
         loop
         muted
       >
-        <source src="/darkside.mp4" type="video/mp4" />
+        <source src="/darkside.png" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
